bdf7bcb60c30406d4ec743cb24781d12a32a14cb
Martin Oviedo <63004949+ndawebs@users.noreply.github.com>
2020-05-12 13:27:05 -0300

    Unnecessary code was removed.
    This template was created with Pinegrow, in this upgrade unnecessary code was removed without Pinegrow's help.

--- functions.php
+++ functions.php
@@ -7,9 +7,7 @@ function esqueleto_setup() {
      * Make theme available for translation.
      * Translations can be filed in the /languages/ directory.
      */
-    /* Pinegrow generated Load Text Domain Begin */
     load_theme_textdomain( 'esqueleto', get_template_directory() . '/languages' );
-    /* Pinegrow generated Load Text Domain End */
 
     // Add default posts and comments RSS feed links to head.
     add_theme_support( 'automatic-feed-links' );
@@ -26,27 +24,13 @@ function esqueleto_setup() {
     set_post_thumbnail_size( 825, 510, true );
 
     // Add menus.
-    register_nav_menus( array(
-        'primary' => __( 'Primary Menu', 'esqueleto' ),
-        'social'  => __( 'Social Links Menu', 'esqueleto' ),
-    ) );
+ //   register_nav_menus( array(
+ //       'primary' => __( 'Primary Menu', 'esqueleto' ),
+  //      'social'  => __( 'Social Links Menu', 'esqueleto' ),
+  //  ) );
 
-/*
-     * Register custom menu locations
-     */
-    /* Pinegrow generated Register Menus Begin */
 
-    /* Pinegrow generated Register Menus End */
-    
-/*
-    * Set image sizes
-     */
-    /* Pinegrow generated Image sizes Begin */
-
-    /* Pinegrow generated Image sizes End */
-    
-    /*
-     * Switch default core markup for search form, comment form, and comments
+     /* Switch default core markup for search form, comment form, and comments
      * to output valid HTML5.
      */
     add_theme_support( 'html5', array(
@@ -70,109 +54,18 @@ endif; // esqueleto_setup
 add_action( 'after_setup_theme', 'esqueleto_setup' );
 
 
-if ( ! function_exists( 'esqueleto_init' ) ) :
-
-function esqueleto_init() {
-
-    
-    // Use categories and tags with attachments
-    register_taxonomy_for_object_type( 'category', 'attachment' );
-    register_taxonomy_for_object_type( 'post_tag', 'attachment' );
-
-    /*
-     * Register custom post types. You can also move this code to a plugin.
-     */
-    /* Pinegrow generated Custom Post Types Begin */
-
-    /* Pinegrow generated Custom Post Types End */
-    
-    /*
-     * Register custom taxonomies. You can also move this code to a plugin.
-     */
-    /* Pinegrow generated Taxonomies Begin */
-
-    /* Pinegrow generated Taxonomies End */
-
-}
-endif; // esqueleto_setup
-
-add_action( 'init', 'esqueleto_init' );
 
 
-if ( ! function_exists( 'esqueleto_custom_image_sizes_names' ) ) :
-
-function esqueleto_custom_image_sizes_names( $sizes ) {
-
-    /*
-     * Add names of custom image sizes.
-     */
-    /* Pinegrow generated Image Sizes Names Begin*/
-    /* This code will be replaced by returning names of custom image sizes. */
-    /* Pinegrow generated Image Sizes Names End */
-    return $sizes;
-}
-add_action( 'image_size_names_choose', 'esqueleto_custom_image_sizes_names' );
-endif;// esqueleto_custom_image_sizes_names
-
-
-
-if ( ! function_exists( 'esqueleto_widgets_init' ) ) :
-
-function esqueleto_widgets_init() {
-
-    /*
-     * Register widget areas.
-     */
-    /* Pinegrow generated Register Sidebars Begin */
-
-    /* Pinegrow generated Register Sidebars End */
-}
-add_action( 'widgets_init', 'esqueleto_widgets_init' );
-endif;// esqueleto_widgets_init
-
-
-
-if ( ! function_exists( 'esqueleto_customize_register' ) ) :
-
-function esqueleto_customize_register( $wp_customize ) {
-    // Do stuff with $wp_customize, the WP_Customize_Manager object.
-
-    /* Pinegrow generated Customizer Controls Begin */
-
-    /* Pinegrow generated Customizer Controls End */
-
-}
-add_action( 'customize_register', 'esqueleto_customize_register' );
-endif;// esqueleto_customize_register
 
 
 if ( ! function_exists( 'esqueleto_enqueue_scripts' ) ) :
     function esqueleto_enqueue_scripts() {
 
-        /* Pinegrow generated Enqueue Scripts Begin */
-
-    /* Pinegrow generated Enqueue Scripts End */
-
-        /* Pinegrow generated Enqueue Styles Begin */
-
-    wp_deregister_style( 'reset' );
-    wp_enqueue_style( 'reset', get_template_directory_uri() . '/css/reset.css', false, null, 'all');
 
     wp_deregister_style( 'style' );
     wp_enqueue_style( 'style', get_bloginfo('stylesheet_url'), false, null, 'all');
 
-    /* Pinegrow generated Enqueue Styles End */
 
     }
     add_action( 'wp_enqueue_scripts', 'esqueleto_enqueue_scripts' );
 endif;
-
-function pgwp_sanitize_placeholder($input) { return $input; }
-/*
- * Resource files included by Pinegrow.
- */
-/* Pinegrow generated Include Resources Begin */
-require_once "inc/wp_pg_helpers.php";
-
-    /* Pinegrow generated Include Resources End */
-?>
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 <?php if ( have_posts() ) : ?>
     <?php while ( have_posts() ) : the_post(); ?>
-        <?php PG_Helper::rememberShownPost(); ?>
         <article> 
             <?php if ( has_post_thumbnail( get_the_ID() ) ) : ?>
                 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/#" alt=""><?php echo PG_Image::getPostImage( get_the_ID(), 'large', null, 'both', null ) ?>

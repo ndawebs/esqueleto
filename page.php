467485b22b0d02acbc5934b2103de2d726e0f5a6
Martin Oviedo <63004949+ndawebs@users.noreply.github.com>
2020-05-30 16:34:51 -0300

    page.php error fixed
    The code for the featured image was removed from the page.php file.

--- page.php
+++ page.php
@@ -3,7 +3,6 @@
 <?php if ( have_posts() ) : ?>
     <?php while ( have_posts() ) : the_post(); ?>
         <article> 
-            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/#" alt=""> 
             <?php the_content(); ?> 
         </article>
     <?php endwhile; ?>
